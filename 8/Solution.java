3413d104d5078016b1112c2fd8906a057e6c61a4
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-03 15:27:08 -0400

    8 solved by each digit

--- 8/Solution.java
+++ 8/Solution.java
@@ -1,56 +1,85 @@
 public class Solution {
     public int myAtoi(String str) {
+        if (str.length() == 0)
+            return 0;
         // INT_MAX (2147483647) or INT_MIN (-2147483648)
         int INT_MAX = 2147483647;
         int INT_MIN = -2147483648;
         int MAX_OVER_10 = 214748364;
+        String string_MAX_OVER_10 = "214748364";
         char ZERO = '0';
-        String s = str.trim();
-        if (s.length() == 0)
-            return 0;
-        int minus = 1;
-        int curr = 0;
-        if (s.charAt(0) == '-'){
-            minus = -1;
-            curr = 1;
-        }
-        else if (s.charAt(0) == '+'){
-            curr = 1;
-        }
-        int val = 0;
 
-        for (; curr < s.length(); curr++){
-            char ch = s.charAt(curr);
-            if (!Character.isDigit(ch))
+        int curr = 0;
+        for (; curr < str.length(); curr++)
+            if (!Character.isWhitespace(str.charAt(curr)))
                 break;
-            if (val < MAX_OVER_10)
+        if (curr == str.length())
+            return 0;
+        boolean positive = true;
+        if (str.charAt(curr) == '-'){
+            positive = false;
+            curr++;
+        }
+        else if (str.charAt(curr) == '+'){
+            curr++;
+        }
+        if (curr == str.length())
+            return 0;
+
+        // ignore successive 0s at the begining;
+        while(str.charAt(curr) == '0')
+            curr++;
+
+        int digitEnd = curr;
+        for (; digitEnd < str.length(); digitEnd++)
+            if (!Character.isDigit(str.charAt(digitEnd)))
+                break;
+        // System.out.println("" + curr + " " + digitEnd);
+        int val = 0;
+        if (digitEnd - curr > 10){
+            if (positive)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        else if (digitEnd - curr < 10) {
+            for (; curr < digitEnd; curr++){
+                char ch = str.charAt(curr);
                 val = val * 10 + (ch - ZERO);
-            else if (val == MAX_OVER_10){
-                if (minus == 1) {
-                    if (ch < '7')
-                        return val * 10 + (ch - ZERO);
-                    else
+            }
+        }
+        else if (digitEnd - curr == 10){
+            boolean safe = false;
+            for (int i = 0; i < 9; i++){
+                char ch = str.charAt(curr + i);
+                if (!safe && ch > string_MAX_OVER_10.charAt(i)){
+                    if (positive)
                         return INT_MAX;
-                }
-                else {
-                    if (ch < '8')
-                        return -(val * 10 + (ch - ZERO));
                     else
                         return INT_MIN;
                 }
+                else if (ch < string_MAX_OVER_10.charAt(i))
+                    safe = true;
+                val = val * 10 + (ch - ZERO);
             }
-            else {
-                if (minus == 1)
+            char ch = str.charAt(curr + 9);
+            // System.out.println(val);
+            // System.out.println(ch);
+            if (!safe) {
+                if (positive && ch >= '7')
                     return INT_MAX;
-                else
+                if (!positive && ch >= '8')
                     return INT_MIN;
             }
+            val = val * 10 + (ch - ZERO);
         }
-        return val * minus;
+        if (!positive)
+            val = -val;
+        return val;
     }
 
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println(solution.myAtoi("-100"));
+        System.out.println(solution.myAtoi(" 1175109307q7"));
     }
 }
