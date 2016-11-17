c89132c4f470eaf7a696fabadd116bf51c3ed33f
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-17 16:32:08 -0500

    17, dfs

--- 17/Solution.java
+++ 17/Solution.java
@@ -2,54 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+    private char[][] mapping = new char[][]{{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},{'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
     public List<String> letterCombinations(String digits) {
+        ArrayList<String> result = new ArrayList<>();
         if (digits.isEmpty())
-            return new ArrayList<String>();
-        int len = digits.length();
-        int N = 1;
-        for(char c: digits.toCharArray()){
-            if (c > '9' || c < '2')
-                throw new RuntimeException();
-            int n = 3;
-            if (c == '9' || c == '7')
-                n = 4;
-            N *= n;
-        }
-        char[][] chars = new char[N][];
-        for (int i = 0; i < N; i++)
-            chars[i] = new char[digits.length()];
-        int blocksize = N;
-        for(int k = 0; k < digits.length(); k++){
-            char c = digits.charAt(k);
-            int n = 3;
-            if (c == '9' || c == '7')
-                n = 4;
-            char[] dic = new char[n];
-            char cStart = (char)('a' + 3  * (c - '2'));
-            if (c > '7')
-                cStart = (char)(cStart + 1);
-            for (int i = 0; i < n; i++)
-                dic[i] = (char)(cStart + i);
-            blocksize /= n;
-            int j = 0;
-            int jj = 0;
-            for (int i = 0; i < N; i++){
-                chars[i][k] = dic[j];
-                jj++;
-                if (jj >= blocksize){
-                    jj = 0;
-                    j++;
-                    if (j >= n)
-                        j = 0;
-                }
-            }
-        }
+            return result;
+        char[] charDigits = digits.toCharArray();
+        char[] charString = new char[digits.length()];
+        dfs(charString, charDigits, result, 0);
+        return result;
+    }
 
-        ArrayList<String> res = new ArrayList<>();
-        for (char[] cs : chars){
-            res.add(new String(cs));
+    private void dfs(char[] charString, char[] charDigits, ArrayList<String> result, int ind){
+        int d = charDigits[ind] - '2';
+        for (char c : mapping[d]){
+            charString[ind] = c;
+            if (ind == charDigits.length - 1)
+                result.add(new String(charString));
+            else
+                dfs(charString, charDigits, result, ind + 1);
         }
-        return res;
     }
 
     public static void main(String[] args){
