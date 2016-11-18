fb39036b77d71885277e0075259b6be99b46e03d
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-18 00:33:55 -0500

    19, one pass solution

--- 19/Solution.java
+++ 19/Solution.java
@@ -1,18 +1,18 @@
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
         int N = 0;
-        ListNode node = head;
-        while(node != null){
-            N++;
-            node = node.next;
-        }
-        if (n == N)
-            return head.next;
+        ListNode front = dummy;
+        ListNode behind = dummy;
+        for (int i = 0; i <= n + 1; i++)
+            front = front.next;
 
-        node = head;
-        for (int i = 0; i < N - n - 1; i++)
-            node = node.next;
-        node.next = node.next.next;
-        return head;
+        while(front != null){
+            front = front.next;
+            behind = behind.next;
+        }
+        behind.next = behind.next.next;
+        return dummy.next;
     }
 }
