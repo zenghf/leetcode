75501d5ff471fa43115ffcbd2802f5396bf13335
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-22 16:59:35 -0500

    29 solved, with many patches

---
+++ 29/Solution.java
@@ -0,0 +1,72 @@
+public class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+        if (divisor == 1)
+            return dividend;
+        if (divisor == -1){
+            if (dividend == Integer.MIN_VALUE)
+                return Integer.MAX_VALUE;
+            else
+                return -dividend;
+        }
+        if (dividend == 0)
+            return 0;
+        if (divisor == Integer.MIN_VALUE){
+            if (dividend == Integer.MIN_VALUE)
+                return 1;
+            else
+                return 0;
+        }
+
+        boolean isPositive = (dividend > 0) == (divisor > 0);
+        int b = divisor > 0 ? divisor : -divisor;
+        int a = 0;
+        int a_remain = 0;
+        if (dividend == Integer.MIN_VALUE){
+            a = Integer.MAX_VALUE - b + 1;
+            a_remain = 1;
+        }
+        else
+            a = dividend > 0 ? dividend : -dividend;
+        int[] array = new int[32];
+        int[] twoPower = new int[32];
+        int i = 0;
+        int num = 1;
+        for (i = 0; i < 32; i++){
+            twoPower[i] = num;
+            num = num + num;
+        }
+        i = 0;
+        num = b;
+        for(i = 0; i < 30 && num <= a; i++){
+            array[i] = num;
+            if (num >= 1073741824)
+                break;
+            num = num + num;
+        }
+        for (int j = 0; j < 32; j++)
+            System.out.println("" + j + " " + array[j]);
+        // i--;
+        if (i >= 30)
+            i = 29;
+        int res = 0;
+        for(; i >= 0 && a > 0 ; i--){
+            if (a >= array[i] && array[i] > 0){
+                System.out.println("" + i + " " + a + " " + array[i] + " " + twoPower[i]);
+                res += twoPower[i];
+                a = a - array[i];
+            }
+        }
+        res = res + a_remain;
+        if (isPositive)
+            return res;
+        else
+            return -res;
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.divide(2, 2));
+    }
+}
