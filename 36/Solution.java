cf2cba5d712e9dbbea393ecfbbb45f34e65f85a9
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-23 15:46:31 -0500

    36 ver 2

--- 36/Solution.java
+++ 36/Solution.java
@@ -1,34 +1,46 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int[] xStart = new int[27];
-        int[] yStart = new int[27];
-        int[] dx = {1, 9, 3};
-        int[] dy = {9, 1, 3};
         for (int i = 0; i < 9; i++){
-            xStart[i] = i;
-            yStart[9 + i] = i;
-            xStart[18 + i] = (i / 3) * 3;
-            yStart[18 + i] = (i % 3) * 3;
-        }
-        for (int i = 0; i < 27; i++){
             boolean[] visited = new boolean[9];
-            int x0 = xStart[i];
-            int y0 = yStart[i];
-            System.out.println("x0: " + x0 + " y0: " + y0);
-            for(int x = x0; x < x0 + dx[i / 9]; x++){
-                for (int y = y0; y < y0 + dy[i / 9]; y++){
-                    char ch = board[y][x];
-                    if (ch == '.')
-                        continue;
-                    System.out.println("x: " + x + " y: " + y);
-                    int k = ch - '1';
-                    if (visited[k])
+            for (int j = 0; j < 9; j++){
+                char ch = board[i][j];
+                if (ch != '.'){
+                    if (visited[ch - '1'])
                         return false;
                     else
-                        visited[k] = true;
+                        visited[ch - '1'] = true;
                 }
             }
         }
+
+        for (int i = 0; i < 9; i++){
+            boolean[] visited = new boolean[9];
+            for (int j = 0; j < 9; j++){
+                char ch = board[j][i];
+                if (ch != '.'){
+                    if (visited[ch - '1'])
+                        return false;
+                    else
+                        visited[ch - '1'] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++){
+            boolean[] visited = new boolean[9];
+            int x0 = (i / 3) * 3;
+            int y0 = (i % 3) * 3;
+            for (int j = 0; j < 9; j++){
+                char ch = board[x0 + j / 3][y0 + j % 3];
+                if (ch != '.'){
+                    if (visited[ch - '1'])
+                        return false;
+                    else
+                        visited[ch - '1'] = true;
+                }
+            }
+        }
+
         return true;
     }
 
@@ -41,7 +53,7 @@ public class Solution {
 
     public static void main(String[] args){
         Solution solution = new Solution();
-        String[] s = {".........","......3..","...18....","...7.....","....1.97.",".........","...36.1..",".........",".......2."};
+        String[] s = {"..4...63.",".........","5......9.","...56....","4.3.....1","...7.....","...5.....",".........","........."};
         for (String ss : s)
             System.out.println(ss);
 
