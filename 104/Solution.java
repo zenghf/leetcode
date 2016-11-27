72588ed888b96871a37ba1cc4d9ac9f255570543
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-27 17:40:30 -0500

    104 bfs

--- 104/Solution.java
+++ 104/Solution.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -11,13 +14,21 @@ public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null)
             return 0;
-        else{
-            int dl = maxDepth(root.left);
-            int dr = maxDepth(root.right);
-            if (dl > dr)
-                return dl + 1;
-            else
-                return dr + 1;
+        Queue<TreeNode> q = new LinkedList<>();
+        TreeNode node = root;
+        q.offer(node);
+        int depth = 0;
+        while(!q.isEmpty()){
+            int n = q.size();
+            for (int i = 0; i < n; i++){
+                node = q.poll();
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            depth++;
         }
+        return depth;
     }
 }
