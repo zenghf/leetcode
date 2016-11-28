e1f273ee2cbb6a5fcdfcea97383bb4f7da96bde3
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-28 14:03:00 -0500

    198 dynamic programming

--- 198/Solution.java
+++ 198/Solution.java
@@ -1,18 +1,11 @@
 public class Solution {
     public int rob(int[] nums) {
-        return rob(nums, 0, nums.length);
-    }
-
-    private int rob(int[] nums, int start, int end){
-        if (start >= end)
-            return 0;
-        if (end - start == 1)
-            return nums[start];
-        if (end - start == 2)
-            return nums[start] > nums[start + 1] ? nums[start] : nums[start + 1];
-        int mid = (start + end) / 2;
-        int rob0 = nums[mid] + rob(nums, start, mid - 1) + rob(nums, mid + 2, end);
-        int rob1 = rob(nums, start, mid) + rob(nums, mid + 1, end);
-        return rob0 > rob1 ? rob0 : rob1;
+        int v0 = 0, v1 = 0, v2 = 0;
+        for (int i = 0; i < nums.length; i++){
+            v2 = v0 + nums[i] > v1 ? v0 + nums[i] : v1;
+            v0 = v1;
+            v1 = v2;
+        }
+        return v2;
     }
 }
