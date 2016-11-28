a0b21ffe5b1e8ea73bfa607d7812f66393d3d983
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-27 20:52:37 -0500

    258 optimized

--- 258/Solution.java
+++ 258/Solution.java
@@ -1,12 +1,8 @@
 public class Solution {
     public int addDigits(int num) {
-        int sum = 0;
-        while(num != 0){
-            sum += num % 10;
-            num = num / 10;
-        }
-        sum = sum % 10 + sum / 10;
-        sum = sum % 10 + sum / 10;
+        int sum = num;
+        while(sum >= 10)
+            sum = sum % 10 + sum / 10;
         return sum;
     }
 }
