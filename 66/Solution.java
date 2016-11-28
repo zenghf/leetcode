aada2d011358c07cfb20ecffa3d4d5d623cdae61
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-28 11:57:56 -0500

    66 optimized

--- 66/Solution.java
+++ 66/Solution.java
@@ -5,22 +5,17 @@ public class Solution {
         if (n == 0)
             return new int[]{1};
         int indFirstNon9 = n - 1;
-        while(indFirstNon9 >= 0 && digits[indFirstNon9] == 9)
-            indFirstNon9--;
-        if (indFirstNon9 == -1){
-            int[] result = new int[n + 1];
-            result[0] = 1;
-            return result;
+        for (; indFirstNon9 >=0; indFirstNon9--){
+            if (digits[indFirstNon9] == 9)
+                digits[indFirstNon9] = 0;
+            else{
+                digits[indFirstNon9]++;
+                return digits;
+            }
         }
-        else{
-            int[] result = new int[n];
-            int i = 0;
-            for (; i < indFirstNon9; i++)
-                result[i] = digits[i];
-            result[i] = digits[i] + 1;
-            for(i = indFirstNon9 + 1; i < n; i++)
-                result[i] = 0;
-            return result;
-        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
     }
 }
