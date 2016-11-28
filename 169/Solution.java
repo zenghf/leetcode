bccdd13ca5553829e4875a2d22ea1f3f0f565b60
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-27 20:44:52 -0500

    169 N

--- 169/Solution.java
+++ 169/Solution.java
@@ -1,7 +1,17 @@
-import java.util.Arrays;
+
 public class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++){
+            if (count == 0){
+                candidate = nums[i];
+            }
+            if (nums[i] == candidate)
+                count++;
+            else
+                count--;
+        }
+        return candidate;
     }
 }
