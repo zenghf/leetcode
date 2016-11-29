db673dfd2864e06c59382285718712a3afbe5331
Haifeng Zeng <zenghaifeng@gmail.com>
2016-11-29 18:00:52 -0500

    217 N time, N space, hash set

--- 217/Solution.java
+++ 217/Solution.java
@@ -1,9 +1,9 @@
-import java.util.Arrays;
+import java.util.HashSet;
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++)
-            if (nums[i] == nums[i - 1])
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums)
+            if (!set.add(num))
                 return true;
         return false;
     }
