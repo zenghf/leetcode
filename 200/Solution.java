b326ae9e5f1aa079e4cc4b22ad81b09c9adb9cae
Haifeng Zeng <zenghaifeng@gmail.com>
2016-12-01 22:03:34 -0500

    200 quick union, very slow

--- 200/Solution.java
+++ 200/Solution.java
@@ -1,4 +1,4 @@
-import java.util.Stack;
+import java.util.HashSet;
 
 public class Solution {
     public int numIslands(char[][] grid) {
@@ -6,31 +6,53 @@ public class Solution {
             return 0;
         int height = grid.length;
         int width = grid[0].length;
-        int[][] islands = new int[height][width];
-        int ind = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int Y = 0; Y < height; Y++){
-            for (int X = 0; X < width; X++){
-                if (grid[Y][X] == '1' && islands[Y][X] == 0){
-                    ind++;
-                    stack.add(Y * width + X);
-                    while(!stack.isEmpty()){
-                        int cord = stack.pop();
-                        int y = cord / width, x = cord % width;
-                        islands[y][x] = ind;
-                        // System.out.println("abc");
-                        if (y > 0 && grid[y - 1][x] == '1' && islands[y - 1][x] == 0)
-                            stack.push((y - 1) * width + x);
-                        if (y < height - 1 && grid[y + 1][x] == '1' && islands[y + 1][x] == 0)
-                            stack.push((y + 1) * width + x);
-                        if (x > 0 && grid[y][x - 1] == '1' && islands[y][x - 1] == 0)
-                            stack.push(y * width + x - 1);
-                        if (x < width - 1 && grid[y][x + 1] == '1' && islands[y][x + 1] == 0)
-                            stack.push(y * width + x + 1);
-                    }
+        int[] id = new int[height * width];
+
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                int v = y * width + x;
+                if (grid[y][x] == '1')
+                    id[v] = v;
+                else
+                    id[v] = -1;
+            }
+        }
+
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                if (grid[y][x] == '1'){
+                    int v = y * width + x;
+                    if (x > 0 && grid[y][x - 1] == '1')
+                        unite(id, v, v - 1);
+                    if (y > 0 && grid[y - 1][x] == '1')
+                        unite(id, v, v - width);
                 }
             }
         }
-        return ind;
+
+        for (int i = 0; i < height * width; i++)
+            if (id[i] != -1)
+                id[i] = root(id, i);
+
+        HashSet<Integer> roots = new HashSet<>();
+        for (int i = 0; i < height * width; i++)
+            if (id[i] != -1)
+                roots.add(id[i]);
+
+        return roots.size();
+    }
+
+    private int root(int[] id, int i){
+        while(i != id[i]){
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    private void unite(int[] id, int p, int q){
+        int i = root(id, p);
+        int j = root(id, q);
+        id[i] = j;
     }
 }
