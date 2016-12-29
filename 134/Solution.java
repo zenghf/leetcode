1720d5d0e6c0de1e152a31184ff29fe083fe1ad8
Haifeng Zeng <zenghaifeng@gmail.com>
2016-12-29 11:33:45 -0500

    134 optimized

--- 134/Solution.java
+++ 134/Solution.java
@@ -1,35 +1,22 @@
 public class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int[] acc = new int[n];
-        int sum = 0;
-        int firstNegative = 0;
-        for (int i = 0; i < n; i++){
-            int temp = gas[i] - cost[i];
-            acc[i] = temp;
-            sum += temp;
-            if (temp < 0)
-                firstNegative = i;
-        }
-        if (acc[firstNegative] > 0)
-            return 0;
-        if (sum < 0)
-            return -1;
-        int negativeUntil = (firstNegative + 1) % n;
-        int totalNegative = acc[firstNegative];
-        int total = 0;
-        int start = negativeUntil;
-        int front = negativeUntil;
-        while (front != firstNegative){
-            total += acc[front];
-            front = (front + 1) % n;
-            if (total < 0){
-                totalNegative += total;
-                negativeUntil = front;
+        int front = 0;
+        int totalFrom0 = 0;
+        int totalFromStart = 0;
+        int start = 0;
+        while (front != n){
+            totalFromStart += (gas[front] - cost[front]);
+            front++;
+            if (totalFromStart < 0){
+                totalFrom0 += totalFromStart;
                 start = front;
-                total = 0;
+                totalFromStart = 0;
             }
         }
-        return start;
+        if (start != n && totalFromStart + totalFrom0 >= 0)
+            return start;
+        else
+            return -1;
     }
 }
