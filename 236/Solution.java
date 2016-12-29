a05efb0cf776fb796b558e3e2b48d8fd36979c91
Haifeng Zeng <zenghaifeng@gmail.com>
2016-12-29 11:19:34 -0500

    236 optimized

--- 236/Solution.java
+++ 236/Solution.java
@@ -8,51 +8,26 @@
  * }
  */
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayDeque<Boolean> pathP = new ArrayDeque<>();
-        ArrayDeque<Boolean> pathQ = new ArrayDeque<>();
-        findPath(root, p, pathP);
-        findPath(root, q, pathQ);
-        // System.out.println(pathP);
-        // System.out.println(pathQ);
-        TreeNode node = root;
-        Iterator<Boolean> P = pathP.iterator();
-        Iterator<Boolean> Q = pathQ.iterator();
-        while(P.hasNext() && Q.hasNext()){
-            boolean pp = P.next();
-            boolean qq = Q.next();
-            if (pp ^ qq)
-                return node;
-            if (pp)
-                node = node.left;
-            else
-                node = node.right;
-        }
-        return node;
-    }
-
-    private boolean findPath(TreeNode root, TreeNode p, Deque<Boolean> path){
         if (root == null)
-            return false;
-        if (root == p)
-            return true;
-
-        path.addLast(true);
-        if (findPath(root.left, p, path))
-            return true;
-        else
-            path.removeLast();
-
-        path.addLast(false);
-        if (findPath(root.right, p, path))
-            return true;
-        else
-            path.removeLast();
-
-        return false;
+            return null;
+        if (root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null){
+            if (right == null)
+                return null;
+            else
+                return right;
+        }
+        else{
+            if (right == null)
+                return left;
+            else
+                return root;
+        }
     }
+
 }
