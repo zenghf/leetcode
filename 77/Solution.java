1ceec2ee10eacb333363397900bb3f6c8ad8032d
Haifeng Zeng <zenghaifeng@gmail.com>
2017-01-06 00:25:54 -0500

    77 optimized

--- 77/Solution.java
+++ 77/Solution.java
@@ -1,34 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        int tc = 1;
-        int nn = n, kk = 1;
-        for (int i = 0; i < k; i++){
-            tc *= nn--;
-            tc /= kk++;
-        }
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         Integer[] ord = new Integer[k];
         for (int i = 0; i < k; i++)
             ord[i] = i + 1;
-        ord[k - 1] = k - 1;
-        for (int i = 0; i < tc; i++){
-            if (ord[k - 1] != n){
-                ord[k - 1]++;
-            }
-            else{
-                int j = k - 1, val = n;
-                while(ord[j] == val){
-                    // System.out.println("j: " + j + " n:" + n);
-                    j--;
-                    val--;
-                }
-                ord[j]++;
-                j++;
-                for (; j < k; j++)
-                    ord[j] = ord[j - 1] + 1;
-            }
-            // System.out.println(Arrays.asList(ord));
+        while (true){
             res.add(new ArrayList<Integer>(Arrays.asList(ord)));
+            int j = k - 1, val = n;
+            while(j >= 0 && ord[j] == val){
+                j--;
+                val--;
+            }
+            if (j < 0)
+                break;
+            ord[j]++;
+            j++;
+            for (; j < k; j++)
+                ord[j] = ord[j - 1] + 1;
         }
         return res;
     }
