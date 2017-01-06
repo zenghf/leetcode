e5d172f8d495318b3c40107ea188fb6718eafb67
Haifeng Zeng <zenghaifeng@gmail.com>
2017-01-05 22:45:20 -0500

    4 solved

--- 4/Solution.java
+++ 4/Solution.java
@@ -1,26 +1,61 @@
+import java.util.ArrayList;
+
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int m1 = n1 / 2;
-        int m2 = n2 / 2;
-        int m = m1 > m2 ? m2 : m1;
-
-    }
-    public double median(int[] nums1, int[] nums2, int start1, int end1, int start2, int end2){
-        f 
-    }
-
-    public void printNums(String hint, int[] nums){
-        System.out.print(hint);
-        for (int i = 0; i < nums.length; i++){
-            System.out.print(" " + nums[i])
+        if (nums1.length > nums2.length){
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
         }
-        System.out.println();
+        int n = nums1.length, m = nums2.length;
+        if (n == 0){
+            if (m % 2 == 1)
+                return nums2[m / 2];
+            return (0.0 + nums2[m / 2] + nums2[m / 2 - 1]) / 2.0;
+        }
+        int half = (n + m) / 2;
+        int low = 0, high = n;
+        int mid = 0;
+        int ind = 0;
+        while (true){
+            mid = (low + high) / 2;
+            ind = half - mid;
+            if (ind < m && mid > 0 && nums1[mid - 1] > nums2[ind]){
+                high = mid - 1;
+            }
+            else if (mid < n && ind > 0 && nums2[ind - 1] > nums1[mid]){
+                low = mid + 1;
+            }
+            else
+                break;
+        }
+
+        System.out.println("mid: " + mid + " ind: " + ind + " n: " + n + " m: " + m);
+        int right = 0;
+        if (mid == n)
+            right = nums2[ind];
+        else if (ind == m)
+            right = nums1[mid];
+        else
+            right = Math.min(nums1[mid], nums2[ind]);
+        if ((n + m) % 2 ==1)
+            return right;
+        int left = 0;
+        if (mid == 0)
+            left = nums2[ind - 1];
+        else if (ind == 0)
+            left = nums1[mid - 1];
+        else
+            left = Math.max(nums1[mid - 1], nums2[ind - 1]);
+
+        return (0.0 + left + right ) / 2.0;
+
     }
 
     public static void main(String[] args){
-        int[] nums1 = new int[]{1, 2, 3, 4};
-        int[] nums2 = new int[]{3, 4, 5};
+        Solution solution = new Solution();
+        int[] nums1 = {1};
+        int[] nums2 = {2, 3};
+        System.out.println(solution.findMedianSortedArrays(nums1, nums2));
     }
 }
