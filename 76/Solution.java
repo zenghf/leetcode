805bce039c66931d896c778d74d70cd7ac9a7ba7
Haifeng Zeng <zenghaifeng@gmail.com>
2017-01-09 10:31:56 -0500

    76 optimized 2

--- 76/Solution.java
+++ 76/Solution.java
@@ -3,29 +3,42 @@ import java.util.*;
 
 public class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, LinkedList<Integer>> dicInd = new HashMap<>();
-        ArrayList<Integer> ind = new ArrayList<Integer>();
         HashMap<Character, Integer> count = new HashMap<>();
+        ArrayList<Integer> ind = new ArrayList<Integer>();
+        HashMap<Character, Integer> T = new HashMap<>();
         for (char ch : t.toCharArray())
-            count.put(ch, count.getOrDefault(ch, 0) + 1);
+            T.put(ch, T.getOrDefault(ch, 0) + 1);
+        // System.out.println(T);
+
+        // HashMap<Character, Integer> S = new HashMap<>();
+        // for (char ch : s.toCharArray())
+        //     S.put(ch, S.getOrDefault(ch, 0) + 1);
+        // System.out.println(S);
+
         int minStart = -1, minEnd = -1, minLen = Integer.MAX_VALUE, nOK = 0;
         int indStart = 0;
         boolean findFirstWindow = false;
+
+        // System.out.println(T.size());
+
         for (int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            if (count.containsKey(ch)){
-                if (!dicInd.containsKey(ch))
-                    dicInd.put(ch, new LinkedList<Integer>());
-                dicInd.get(ch).add(i);
-                if (dicInd.get(ch).size() == count.get(ch))
-                    nOK++;
-                if (!findFirstWindow && nOK == count.size())
-                    findFirstWindow = true;
+            if (T.containsKey(ch)){
                 ind.add(i);
+                count.put(ch, count.getOrDefault(ch, 0) + 1);
+                if (!findFirstWindow){
+                    if (count.get(ch).equals(T.get(ch)))
+                        nOK++;
+                    // if (ch == 'a')
+                    //     System.out.println("" + count.get(ch) + "   " + T.get(ch) + "   " + nOK);
+                    // System.out.println("" + count + "\n\t" + nOK);
+                    if (nOK == T.size())
+                        findFirstWindow = true;
+                }
                 if (findFirstWindow){
                     char first = s.charAt(ind.get(indStart));
-                    while(dicInd.get(first).size() > count.get(first)){
-                        dicInd.get(first).pollFirst();
+                    while(count.get(first) > T.get(first)){
+                        count.put(first, count.get(first) - 1);
                         indStart++;
                         first = s.charAt(ind.get(indStart));
                     }
@@ -40,14 +53,19 @@ public class Solution {
         }
         if (!findFirstWindow)
             return "";
-        System.out.println(dicInd);
-        System.out.println(ind);
+        // System.out.println(dicInd);
+        // System.out.println(ind);
 
         return s.substring(minStart, minEnd + 1);
     }
 
     public static void main(String[] args){
         Solution solution = new Solution();
-        System.out.println(solution.minWindow("acbbaca", "aba"));
+        Scanner in = new Scanner(System.in);
+        // String s = in.nextLine();
+        // String t = in.nextLine();
+        String s = "a";
+        String t = "a";
+        System.out.println(solution.minWindow(s, t));
     }
 }
