594adf1c448ee9ba3c51ea4909ed0ba07e8377f1
Haifeng Zeng <zenghaifeng@gmail.com>
2017-01-09 09:28:44 -0500

    76 optimized

--- 76/Solution.java
+++ 76/Solution.java
@@ -8,42 +8,41 @@ public class Solution {
         HashMap<Character, Integer> count = new HashMap<>();
         for (char ch : t.toCharArray())
             count.put(ch, count.getOrDefault(ch, 0) + 1);
-        int start = -1, end = -1, nOK = 0;
+        int minStart = -1, minEnd = -1, minLen = Integer.MAX_VALUE, nOK = 0;
+        int indStart = 0;
+        boolean findFirstWindow = false;
         for (int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             if (count.containsKey(ch)){
-                if (dicInd.isEmpty())
-                    start = i;
                 if (!dicInd.containsKey(ch))
                     dicInd.put(ch, new LinkedList<Integer>());
                 dicInd.get(ch).add(i);
                 if (dicInd.get(ch).size() == count.get(ch))
                     nOK++;
-                if (end == -1 && nOK == count.size())
-                    end = i;
+                if (!findFirstWindow && nOK == count.size())
+                    findFirstWindow = true;
                 ind.add(i);
+                if (findFirstWindow){
+                    char first = s.charAt(ind.get(indStart));
+                    while(dicInd.get(first).size() > count.get(first)){
+                        dicInd.get(first).pollFirst();
+                        indStart++;
+                        first = s.charAt(ind.get(indStart));
+                    }
+                    int len = i + 1 - ind.get(indStart);
+                    if (minStart == -1 || len < minLen){
+                        minStart = ind.get(indStart);
+                        minEnd = i;
+                        minLen = len;
+                    }
+                }
             }
         }
+        if (!findFirstWindow)
+            return "";
         System.out.println(dicInd);
         System.out.println(ind);
-        if (start == -1 || end == -1)
-            return "";
-        int minLen = end - start + 1, minStart = start, minEnd = end;
-        System.out.println("minLen: " + minLen + " start: " + start + " end: " + end);
-        for(int indStart = 1; indStart < ind.size(); indStart++){
-            char ch = s.charAt(ind.get(indStart - 1));
-            LinkedList<Integer> list = dicInd.get(ch);
-            list.pollFirst();
-            if (list.size() < count.get(ch))
-                break;
-            start = ind.get(indStart);
-            end = Math.max(end, list.get(count.get(ch) - 1));
-            if (end + 1 - start < minLen){
-                minStart = start;
-                minEnd = end;
-                minLen = end + 1 - start;
-            }
-        }
+
         return s.substring(minStart, minEnd + 1);
     }
 
